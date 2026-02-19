58d2d8b768660e688d1c50b13f546b98e6d25b89
JassK0 <jasskahlon08@gmail.com>
2026-02-19 00:51:56 -0500

    n

--- api/main.py
+++ api/main.py
@@ -2,10 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return {"ok": True}
-
-@app.get("/health")
+@app.get("/api/health")
 def health():
     return {"health": "ok"}
